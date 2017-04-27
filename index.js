9b5d9bd32caae1f0beefe838ab70a37421d34470
Bryson Kruk <brysonmk1984@gmail.com>
2017-04-26 18:28:24 -0600

    initial working version

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ pg.defaults.ssl = true;
 const productionMode = true;
 const dbUrl = productionMode ? process.env.DATABASE_URL : "postgres:///localchatwall";
 
-/*let messages = [
-    { 'name' : 'Bryson', 'id' : 0, 'message' : 'You Suck!' },
-    { 'name' : 'Ryan', 'id' : 1, 'message' : 'No You Suck!' },
-    { 'name' : 'Matt', 'id' : 2, 'message' : 'Both of you suck' }
-];*/
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(function(req, res, next) {
@@ -25,31 +19,16 @@ app.use(function(req, res, next) {
 });
 app.use(express.static("./public"));
 app.use(cors());
-app.use(function (err, req, res, next) {
-  console.error('err stack'.err.stack)
-  res.status(500).send('Something broke!')
-})
-
-
-// TEST GET @ /DB
-app.get('/db', function (req, res) {
-  pg.connect(dbUrl, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-        if (err) { console.error(err); res.send("DB CB Error " + err); 
-         }else{ 
-             console.log('results row',result.rows);
-              res.status(200).json({ 'messages': result.rows });
-        }
-    });
-  });
-});
 
 // GET MESSAGES
 app.get('/chat-wall-api', function (req, res) {
     pg.connect(dbUrl, function(err, client, done) {
+        if (err) { 
+            console.error(err)
+            res.send("DB CONNECTION Error " + err); 
+        }
         client.query('SELECT * FROM test_table', function(err, result) {
-        done();
+            done();
             if (err) { console.error(err); res.send("DB CB Error " + err); 
             }else{ 
                 console.log('results row',result.rows);
@@ -67,37 +46,56 @@ app.post('/chat-wall-api', function(req, res){
              res.json({ 'pushSuccess' : false });
             throw err;
         }
-        client.query(`INSERT INTO test_table(name, message) VALUES ('${req.query.name}', '${req.query.message}')`, function(err, result) {
-        done();
-        if(err){
-             res.json({ 'pushSuccess' : false });
-            throw err;
+        if(req.query.name && req.query.message){
+            client.query(`INSERT INTO test_table(name, message) VALUES ('${req.query.name}', '${req.query.message}')`, function(err, result) {
+                done();
+                if(err){
+                    res.json({ 'postSuccess' : false });
+                    throw err;
+                }
+                client.query('SELECT * FROM test_table', function(err, result) {
+                    done();
+                    if (err) { res.json({ 'postSuccess' : false }); throw err;
+                    }else{ 
+                        console.log('results row',result.rows);
+                        res.status(200).json({ 'messages': result.rows, 'postSuccess' : true });
+                    }
+                });
+            });
+        } else {
+            throw new Error('Name and Message must be provided');
         }
-        console.log('pushed');
-         res.json({ 'pushSuccess' : true });
-        });
     });
    
 });
 
 // DELETE MESSAGE
 app.delete('/chat-wall-api/:id', function(req, res){
-    let match = messages.find((message)=>{
-        return message.id === parseInt(req.params.id);
+    pg.connect(dbUrl, function(err, client, done) {
+        if (err) { 
+            res.json({ 'deleteSuccess' : false });
+            throw err; 
+        }
+        client.query(`DELETE FROM test_table WHERE '${req.params.id}' = test_table.id `, function(err, result) {
+            done();
+            if (err) { res.json({ 'deleteSuccess' : false }); throw err; 
+            }else{ 
+                client.query('SELECT * FROM test_table', function(err, result) {
+                    done();
+                    if (err) { console.error(err); res.send("DB CB Error " + err); 
+                    }else{ 
+                        console.log('results row',result.rows);
+                        res.status(200).json({ 'messages': result.rows, 'deleteSuccess' : true });
+                    }
+                });
+            }
+        });
     });
-    let indexMatch = messages.indexOf(match);
-    messages.splice(indexMatch,1);
-    res.json({ 'messages': messages });
 });
 
 
 // SERVER TYPE
 if(productionMode){
-    // Production https server
-    /*https.createServer(app).listen( process.env.PORT, function(){
-        console.log('Https App started on ', process.env.PORT);
-    });*/
-
     // Production http server
     app.listen( process.env.PORT, function () {
         console.log('Listening on port ' + process.env.PORT);
